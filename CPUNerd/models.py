a81dc335e7b3eda2f3e939930386dded2138d500
chenyongzhuang1996@163.com <zhphuang@qumitech.com>
2020-03-08 20:03:42 +0800

    add cpumodel

--- CPUNerd/models.py
+++ CPUNerd/models.py
@@ -1,3 +1,19 @@
 from django.db import models
 
 # Create your models here.
+
+
+class CPUModel(models.Model):
+    description = models.CharField(max_length=255)
+    image = models.ImageField(max_length=1000, upload_to='avatar', null=True, blank=True)
+    mark = models.IntegerField()
+    category = models.IntegerField(choices=((0, 'AMD'), (1, 'Intel')))
+    price = models.IntegerField()
+    purpose = models.IntegerField(choices=((0, 'Entertainment'), (1, 'office'), (2, 'simplicity'), (3, 'game')))
+    core = models.IntegerField(choices=((0, 'four hz'), (1, 'six hz'), (2, 'eight hx'), (4, 'ten hx')))
+    frequency = models.FloatField()
+
+
+
+
+
